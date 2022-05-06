ffc3490f33653e13c0fb483d9d01fedaa3a5d480
cyzkrau <cyzkrau@gmail.com>
2022-05-06 15:07:17 +0800

    22-05-06-update picture

--- newtime.py
+++ newtime.py
@@ -4,14 +4,14 @@ import numpy as np
 import time
 
 # 安康
-bbox = [87, 293, 277, 313, 20]  # 安康码时间位置(:4)以及字体大小(-1)
+bbox = [133, 550, 440, 585, 33]  # 安康码时间位置(:4)以及字体大小(-1)
 img = cv2.imread("akm.jpg")
+shp = (bbox[2] - bbox[0], bbox[3] - bbox[1])
 im = Image.new("RGB", (bbox[2] - bbox[0], bbox[3] - bbox[1]), (254, 254, 254))
 dr = ImageDraw.Draw(im)
 font = ImageFont.truetype("arial.ttf", bbox[4])
-
 dr.text((0, 0), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), (0, 0, 0), font)
-img[bbox[1] : bbox[3], bbox[0] : bbox[2], :] = cv2.resize(np.array(im), (190, 20))
+img[bbox[1] : bbox[3], bbox[0] : bbox[2], :] = cv2.resize(np.array(im), shp)
 cv2.imwrite("akm.jpg", img)
 
 # 行程
